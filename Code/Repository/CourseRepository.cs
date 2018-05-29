f5194ae51929147bdb8cdd5bfd3394b436473d04
Hasan Khan <hasan.khan.uap@gmail.com>
2018-05-29 23:26:05 +0600

    Create CourseRepository.cs

---
+++ Code/Repository/CourseRepository.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Quries.Persistence.Repositories
+namespace ConsoleApp.Repository
+{
+    public class CourceRepository: Repository<Course>, IRepository
+    {
+        public CourceRepository(PlutoContext context): base(context)
+        {
+
+        }
+
+        public IEnumerable<Course> GetTopSellingCourses(int count)
+        {
+            return PlutoContext.Courses.OrderByDescending(c => c.FullPrice).Take(count).ToList();
+        }
+
+        public IEnumerable<Course> GetCoursesWithAuthors(int pageIndex, int pageSize)
+        {
+            return PlutoContext.Courses
+                .InClude(s => s.Author)
+                .OrderBy(s => s.Name)
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+        }
+
+        public PlutoContext PlutoContext
+        {
+            get { return Context as PlutoContext}
+        }
+    }
+}
