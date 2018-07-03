716a81ab1f98d859b0b4dc93e9362ce034b085bf
Hasan Khan <hasan.khan.uap@gmail.com>
2018-07-03 23:01:55 +0600

    Core1

---
+++ Projects/Core1/Core1/Pages/Create.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Core1.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public CreateModel(AppDbContext db) { _db = db; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Customers.Add(Customer);
+            await _db.SaveChangesAsync();
+            Message = $"Customer {Customer.Id} added!";
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ Projects/Core1/Core1/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public IndexModel(AppDbContext db) { _db = db; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public IList<Customer> Customers { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            Customers = await _db.Customers.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var custmer = await _db.Customers.FindAsync(id);
+            if (custmer != null)
+            {
+                _db.Customers.Remove(custmer);
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToPage();
+        }
+    }
+}
